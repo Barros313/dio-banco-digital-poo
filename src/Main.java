72fcc1297414792e24f01e68ceb4a28239b06369
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-06-25 10:59:27 -0300

    Testing class

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        ContaCorrente conta = new ContaCorrente();
+        ContaPoupanca poupanca = new ContaPoupanca();
+
+        System.out.println(conta.toString());
+        System.out.println(poupanca.toString());
+    }
+}
