40dad131d87169378c7f0274447efc7ffacad330
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-06-26 09:55:46 -0300

    Implemented address class to bank

--- src/com/banco/estabelecimento/Banco.java
+++ src/com/banco/estabelecimento/Banco.java
@@ -2,12 +2,18 @@ package com.banco.estabelecimento;
 
 public class Banco {
     private String nome;
+    private Endereco endereco;
+
+    public Banco(String nome, Endereco endereco) {
+        this.nome = nome;
+        this.endereco = endereco;
+    }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void getEndereco() {
+        endereco.imprimirEnderecoFormatado();
     }
 }
